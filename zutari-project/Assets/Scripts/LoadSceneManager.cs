8a4ba1b1f35ca3130a1955b09641cde26cf657b2
Michael <michaelsengelbrecht@gmail.com>
2023-03-28 20:28:51 +0200

    Zutari | Level One | Set camera to orthographic
    Set camera to orthographic in "Level One" scene

--- zutari-project/Assets/Scripts/LoadSceneManager.cs
+++ zutari-project/Assets/Scripts/LoadSceneManager.cs
@@ -17,13 +17,13 @@ public class LoadSceneManager : MonoBehaviour
 
     }
 
-    // Loads a scene based on Index in build settings
+    // Loads a scene based on Index, assuming that scene is added into build settings
     public void LoadSceneWithIndex(int _sceneIndex)
     {
        SceneManager.LoadScene(_sceneIndex);
     }
 
-    // Loads a scene based on Scene Name
+    // Loads a scene based on Scene Name, assuming that scene is added into build settings
     public void LoadSceneWithString(string _sceneName)
     {
         SceneManager.LoadScene(_sceneName);
