7f320235c53163ad35ab66196fcd059754401797
Michael <michaelsengelbrecht@gmail.com>
2023-03-28 21:17:55 +0200

    Zutari | Level One | Added functionality to change cube color based on direction
    Added functionality to change cube color based on direction of player movement.

--- zutari-project/Assets/PlayerController.cs
+++ zutari-project/Assets/PlayerController.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Player Settings")]
-    [SerializeField] private Rigidbody rb;
     [SerializeField, Range(1f, 1000)] private float playerMovementSpeed = 1;
     [SerializeField, Range(0.01f, 100)] private float playerDragForce = 1;
+    [SerializeField] private bool movingLeft, movingRight, movingUp, movingDown;
+
+    private Rigidbody rb;
+    private Material playerCubeRenderer;
+
     // Awake is called upon pressing play before the first frame is rendered
     private void Awake()
     {
+        playerCubeRenderer = GetComponent<Renderer>().sharedMaterial;
         rb = GetComponent<Rigidbody>();
-        if(rb != null)
-        {
-            rb.drag = playerDragForce;
-            rb.angularDrag = playerDragForce;
-        }
     }
 
     // Start is called before the first frame update
@@ -29,39 +29,101 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerInput();
+        PlayerAdjustDrag();
     }
 
-    // Called every frame to register player input. Used to move up, down, left, and right using WASD
+    // Called every frame to register player input. Used to move up, down, left, and right using WASD (Supports diagonal movement as well)
     public void PlayerInput()
     {
+        // Moves Player UP
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("Player moving up");
+            //Debug.Log("Player moving up");
             rb.AddForce(Vector3.forward * playerMovementSpeed, ForceMode.Impulse);
+            movingUp = true;
         }
-
-        if (Input.GetKey(KeyCode.A))
+        else
         {
-            Debug.Log("Player moving left");
-            rb.AddForce(-Vector3.right * playerMovementSpeed, ForceMode.Impulse);
+            movingUp = false;
         }
 
+        // Moves Player LEFT
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W))
+        {
+            //Debug.Log("Player moving left");
+            rb.AddForce(-Vector3.right * playerMovementSpeed, ForceMode.Impulse);
+            movingLeft = true;
+        }
+        else
+        {
+            movingLeft = false;
+        }
+
+        // Moves Player DOWN
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("Player moving down");
+            //Debug.Log("Player moving down");
             rb.AddForce(-Vector3.forward * playerMovementSpeed, ForceMode.Impulse);
+            movingDown = true;
+        }
+        else
+        {
+            movingDown = false;
         }
 
+        // Moves Player RIGHT
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Player moving right");
+            //Debug.Log("Player moving right");
             rb.AddForce(Vector3.right * playerMovementSpeed, ForceMode.Impulse);
+            movingRight = true;
+        }
+        else
+        {
+            movingRight = false;
         }
 
+        // Calls method to change color based on player input
+        ChangeMaterialColor();
     }
 
+    // Adjusts Color based on player input of direction
     public void ChangeMaterialColor()
     {
+        if (movingUp == true)
+        {
+            playerCubeRenderer.color = Color.red;
+        }
 
+        if (movingLeft == true)
+        {
+            playerCubeRenderer.color = Color.blue;
+        }
+
+        if (movingDown == true)
+        {
+            playerCubeRenderer.color = Color.yellow;
+        }
+
+        if (movingRight == true)
+        {
+            playerCubeRenderer.color = Color.cyan;
+        }
+
+        // No input detected will make cube black
+        if (!Input.anyKey)
+        {
+            playerCubeRenderer.color = Color.black;
+        }
+    }
+
+    // Adjust Drag of Rigidbody in Update based on settings of player
+    public void PlayerAdjustDrag()
+    {
+        if (rb != null)
+        {
+            rb.drag = playerDragForce;
+            rb.angularDrag = playerDragForce;
+        }
     }
 }
